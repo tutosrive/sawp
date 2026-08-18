74624287fb29b2aaf9a3b8bb2c1dfad132c4dbda
tutosrive <tutosrive@gmail.com>
2026-08-18 18:26:11 -0500

    dev: Functional client for Postgres using supabase string connection

--- app/supabase/run.ts
+++ app/supabase/run.ts
@@ -1,3 +1,7 @@
+import DB from '../database/db.ts'
+
 export default async function runs(data){
+    const db = new DB()
     console.log(`Running supabase with data from user "${data.user.login}" ...`)
+    await db.addData('test1', null)
 }
