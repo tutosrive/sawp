742909166a3b3e1f1807146488344279477bf1f0
tutosrive <tutosrive@gmail.com>
2026-08-19 21:46:16 -0500

    dev: The previous commit message was for here HAHAHHAA wtf

--- app/supabase/query.ts
+++ app/supabase/query.ts
@@ -1,33 +1,43 @@
-//import {writeFile} from 'node:fs/promises'
-
-// TODO: Missing make combined fields like 'readme_url'
+import {writeFile} from 'node:fs/promises'
 
 export default function createInsertQuery(data){
-    const tables = ['admin', 'owner', 'language', 'repository']
-    const {admin, users, languages, repositories} = parseData(data)
-    let query = `INSERT INTO admin VALUES ${admin};
-    INSERT INTO owner VALUES ${users};`
-    //INSERT INTO language VALUES ${languages};
-    //INSERT INTO repository VALUES ${repositories};`
-    //await writeFile('app/test.txt', query, 'utf8')
+    const {admin, users, languages, repositories, topics, topicsXrepo, licenses} = parseData(data)
+    const tables = [
+        {name: 'admin', data: admin},
+        {name: 'owner', data: users},
+        {name: 'language', data: languages},
+        {name: 'repository', data: repositories},
+        {name: 'licenses', data: licenses}
+    ]
+    let query = ''
+    tables.forEach(table => {query += `INSERT INTO ${table.name} VALUES ${table.data};`})
+    const write = async ()=> {await writeFile('app/test.txt', query, 'utf8')}
+    write()
     return query.replaceAll(',)', ')')
 }
 
-function parseData(data){
+async function parseData(data){
+    data.user.stargazerCount = data.repositories.stargazerCount
+    delete data.repositories.stargazerCount
     const admin = plainObject(data.user)
+    let topics = arrayPlain(data.reposTopics)
+    let topicsXrepo = arrayPlain(data.topicsXrepo)
+    let licenses = arrayPlain(data.licenses)
     let langs = []
     let owners = []
     let repos = []
     
     for(let repo of data.repositories){
+        repo.primaryLanguageId = "none"
+        repo.readmeUrl = await parseUrlReadme(repo.name, repo.owner.login)
         const lang = repo.primaryLanguage
         const owner = repo.owner
-        if(lang !== undefined && lang !== null){
+        if((lang !== undefined) && (lang !== null)){
             let langExist: boolean = langs.some(l => l.id === lang.id)
             if(langExist === false) langs.push(lang)
             repo.primaryLanguageId = lang.id
         }
-        if(owner !== undefined && owner !== null){
+        if((owner !== undefined) && (owner !== null)){
             let ownerExist: boolean = owners.some(l => l.id === owner.id)
             if(ownerExist === false) owners.push(owner)
             repo.ownerId = owner.id
@@ -40,7 +50,7 @@ function parseData(data){
     const repositories = arrayPlain(repos)
     const languages = arrayPlain(langs)
 
-    return {admin, users, languages, repositories}
+    return {admin, users, languages, repositories, topics, topicsXrepo, licenses}
 }
 
 function arrayPlain(arr){
@@ -61,12 +71,18 @@ function plainObject(obj): string {
     for(let key in obj) {
         let value = obj[key]
         if((typeof value) === 'string'){
-            str += `'${value}',`
+            str += `${key}:'${value}',`
         } else {
-            str += `${value},`
+            str += `${key}:${value},`
         }
     }
     str +=')'
 
     return str
+}
+
+async function parseUrlReadme(reponame:string, ownername:string):Promise<string>{
+    const req = await fetch(`https://api.github.com/${reponame}/${ownername}`)
+    const url = await req.json().download_url
+    return url
 }
--- app/supabase/run.ts
+++ app/supabase/run.ts
@@ -2,12 +2,24 @@ import DB from '../database/db.ts'
 import createInsertQuery from './query.ts'
 
 export default async function runs(data){
-    const db = new DB()
     console.log(`Running supabase with data from user "${data.user.login}" ...`)
+    const db = new DB()
+    console.log('Creating database tables')
+    await db.createTables((e, r) => {dispatchQueries(db, e, r)})
     const queryInserts = createInsertQuery(data)
-    let timeout = setTimeout(async()=>{
-        await db.addData(queryInserts)
-        timeout = null
+    //let timeout = setTimeout(async()=>{
+        //await db.runQuery(queryInserts)
+        //timeout = null
         //await db.closeClient()
-    }, 30000)
+    //}, 10000)
+}
+
+async function dispatchQueries(db, err, res){
+    if((err !== undefined) && (err !== null)){
+        console.log('Not error')
+        return
+    }
+    console.log('error')
+    console.log(err, res)
+    db.closeClient()
 }
